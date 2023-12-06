140a533acc0c0838ab8f39040605fce80bc48c79
AdithyaBNayak <1999adithya@gmail.com>
2023-12-06 22:47:26 +0530

    Final changes

--- backend/handler.py
+++ backend/handler.py
@@ -1,18 +1,14 @@
-import json
+import time
 import boto3
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-    boto3_version = boto3.__version__
-    print(f"Boto3 Version: {boto3_version}")
-
+    
+    prompt = event['arguments']['prompt']
+    
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        'response': prompt,
+        'createdAt': str(time.time()) 
     }
 
     return response
